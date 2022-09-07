4e5b79aa72f409da8cefbd708c0b38282ae31d11
azmeer <m.azmeer2003@gmail.com>
2022-09-07 19:26:29 +0500

    Committing Tasks 1-5.

---
+++ HR4.py
@@ -0,0 +1,20 @@
+'''if __name__ == '__main__':
+    n = int(input())
+    if n < 0:
+        exit()
+
+    else:
+    
+        i = 0
+    while i <= n:
+        print (i**2,"\n")
+        i = i + 1'''
+
+if __name__ == '__main__':
+    n = int(input())
+    
+    
+    i = 0
+    while i <= n  and n >= 0:
+        print (i**2,"\n")
+        i = i + 1
