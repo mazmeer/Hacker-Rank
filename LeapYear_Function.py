f51f8def7826d58178109400e1214daeffc8927e
azmeer <m.azmeer2003@gmail.com>
2022-09-11 17:15:07 +0500

    Renamed and added more solutions.

--- LeapYear_Function.py
+++ LeapYear_Function.py
@@ -2,13 +2,16 @@ def is_leap(year):
     Leap = False
     
     
-    if year%400==0 :
+    if year%400 == 0 :
         Leap = True
+        print(Leap)
 
     elif year%4 == 0 and year%100 != 0:
         Leap = True
+        print(Leap)
 
+    
     return Leap
+    
 
 year = int(input())
-print(is_leap(year))
