13424c64e6339308a934e210c6a555457c42d4e5
azmeer <m.azmeer2003@gmail.com>
2022-09-16 12:15:36 +0500

    )))

---
+++ Text_Alignment.py
@@ -0,0 +1,11 @@
+thickness = int(input()) #This must be an odd number
+c = 'H'
+
+#Top Cone
+for i in range(thickness):
+    print((c*i).rjust(thickness-1)+c+(c*i).ljust(thickness-1))
+
+#Top Pillars
+for i in range(thickness+1):
+    print((c*thickness).center(thickness*2)+(c*thickness).center(thickness*6))
+
