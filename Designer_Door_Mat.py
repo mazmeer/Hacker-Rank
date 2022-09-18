b409db25498eaf25a23685b3561e092f056fbbb0
azmeer <m.azmeer2003@gmail.com>
2022-09-18 15:29:22 +0500

    18th September.

--- Designer_Door_Mat.py
+++ Designer_Door_Mat.py
@@ -2,13 +2,13 @@ n,m = input().split()
 n = int(n)
 m = int(m)
 
-#printing first half
+
 for i in range(n//2):
     t = int((2*i)+1)
     print(('.|.'*t).center(m, '-'))
-#printing middle line
+
 print('WELCOME'.center(m,'-'))
-#printing last half
-'''for i in reversed(range(n//2)):
+
+for i in reversed(range(n//2)):
     t = int((2*i)+1)
-    print(('.|.'*t).center(m, '-'))'''
+    print(('.|.'*t).center(m, '-'))
